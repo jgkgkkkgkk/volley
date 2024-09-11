a82a1dc5fef5b75da01f9e9c0dda2b9f4e128196
Pavel Vasilinschii <pascha1998ga@mail.ru>
2024-09-11 18:07:29 +0300

    created member list

--- src/main/java/com/volley/controller/MemberController.java
+++ src/main/java/com/volley/controller/MemberController.java
@@ -1,8 +1,11 @@
 package com.volley.controller;
 
 import com.volley.dto.MemberDto;
+import com.volley.entities.Hollyday;
 import com.volley.entities.Member;
 import com.volley.entities.Team;
+import com.volley.exceptions.NotFoundException;
+import com.volley.repo.MemberRepo;
 import com.volley.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -14,12 +17,21 @@ import java.util.List;
 public class MemberController {
     @Autowired
     private MemberService memberService;
+    @Autowired
+    private MemberRepo memberRepo;
 
     @PostMapping("/save")
     public void insertMember(@RequestBody MemberDto member) {
         memberService.createNewMember(member);
     }
 
+//    public Member getMemberById(Integer id) {
+//        return memberRepo.findById(id).orElseThrow(NotFoundException::new);
+//    }
+    @PutMapping("/{id}")
+    public Member updateMemberById(@PathVariable Integer id, @RequestBody MemberDto memberDto) {
+        return memberService.updateMemberById(id, memberDto);
+    }
 
     @GetMapping("/list")
     public List<MemberDto> findAllMembers() {
--- src/main/java/com/volley/service/MemberService.java
+++ src/main/java/com/volley/service/MemberService.java
@@ -4,6 +4,7 @@ import com.volley.dto.MemberDto;
 import com.volley.entities.Member;
 import com.volley.entities.Team;
 import com.volley.exceptions.EmailAlreadyExist;
+import com.volley.exceptions.NotFoundException;
 import com.volley.repo.MemberRepo;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -20,7 +21,9 @@ public class MemberService {
     private final TeamService teamService;
 
     public List<Member> getAllMembers() {return memberRepo.findAll();}
-
+    public Member getMemberById(Integer id) {
+        return memberRepo.findById(id).orElseThrow(NotFoundException::new);
+    }
     public void createNewMember(MemberDto createMemberDto) {
         Optional<Member> dbMember = memberRepo.findByEmail(createMemberDto.getEmail());
         if (dbMember.isPresent()) {
@@ -32,5 +35,21 @@ public class MemberService {
 
     public void deleteMemberById(Integer id) {
         memberRepo.deleteById(id);
+    }
+
+
+
+    public Member updateMemberById(Integer id, MemberDto memberClient) {
+        Team teamById = teamService.getTeamById(memberClient.getTeamId());
+
+        Member memeber = getMemberById(id);
+        memeber.setLastname(memberClient.getLastname());
+        memeber.setFirstname(memberClient.getFirstname());
+        memeber.setAge(memberClient.getAge());
+        memeber.setHeight(memberClient.getHeight());
+        memeber.setEmail(memberClient.getEmail());
+        memeber.setTeam(teamById);
+
+        return memberRepo.save(memeber);
     }
 }
